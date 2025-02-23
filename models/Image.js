65371c4e75e51316a112cd992f5ab686868743c5
Sourav Das <94233240+Dassourav07@users.noreply.github.com>
2025-02-24 00:39:52 +0530

    Update Image.js

--- models/Image.js
+++ models/Image.js
@@ -1,12 +1,29 @@
 const mongoose = require('mongoose');
 
-const imageSchema = new mongoose.Schema({
-  requestId: { type: String, required: true },
-  serialNumber: { type: Number, required: true },
-  productName: { type: String, required: true },
-  inputUrl: { type: String, required: true },
-  outputUrl: { type: String },
-  status: { type: String, enum: ['Pending', 'Completed', 'Failed'], default: 'Pending' },
+const ImageSchema = new mongoose.Schema({
+  requestId: {
+    type: String,
+    required: true,
+  },
+  serialNumber: {
+    type: Number,
+    required: true,
+  },
+  productName: {
+    type: String,
+    required: true,
+  },
+  inputUrl: {
+    type: String,
+    required: true,
+  },
+  outputUrl: {
+    type: String,
+  },
+  status: {
+    type: String,
+    default: 'Pending',
+  },
 });
 
-module.exports = mongoose.model('Image', imageSchema);
+module.exports = mongoose.model('Image', ImageSchema);
